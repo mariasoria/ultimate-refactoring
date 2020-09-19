0bf293ee2a3d61ecc7ddef23bace3572baab8400
Marieta <maria.it3@gmail.com>
2020-09-19 12:00:52 +0100

    Refactored Entity class

--- src/main/java/untestableLegacy/Entity.java
+++ src/main/java/untestableLegacy/Entity.java
@@ -22,44 +22,61 @@ public class Entity {
 	  }
 
 	private String removeIllegalChars(String fromName) {
-		final char[] chars = fromName.toCharArray();
+		char[] chars = getCharsFromName(fromName);
 		final StringBuilder newName = new StringBuilder();
 		boolean nameChanged = false;
+		nameChanged = ChangesNameIfIllegalChars(chars, newName, nameChanged);
+		if (nameChanged) {
+		  warnLogger("The entity name " + fromName
+				  + " contains illegal characters, it has been repaired to " + newName);
+		} else {
+		  // check for other less normal characters
+			checksNotNormalChars(fromName);
+		}
+		return newName.toString();
+	  }
+
+	private char[] getCharsFromName(String fromName) {
+		final char[] chars = fromName.toCharArray();
+		return chars;
+	}
+
+	private boolean ChangesNameIfIllegalChars(char[] chars, StringBuilder newName, boolean nameChanged) {
 		for (char c : chars) {
 		  boolean hasIllegalChar = false;
-		  final char[] ILLEGAL_ENTITY_NAME_CHARS = new char[]{'*', '?'};
-		  for (char illegalChar : ILLEGAL_ENTITY_NAME_CHARS) {
-			if (c == illegalChar) {
-			  hasIllegalChar = true;
-			  break;
+			char[] ILLEGAL_ENTITY_NAME_CHARS = new char[]{'*', '?'};
+			for (char illegalChar : ILLEGAL_ENTITY_NAME_CHARS) {
+				if (c == illegalChar) {
+					hasIllegalChar = true;
+					break;
+				}
 			}
-		  }
-		  if (hasIllegalChar) {
+			if (hasIllegalChar) {
 			nameChanged = true;
 			continue;
 		  }
 		  newName.append(c);
 		}
-		if (nameChanged) {
-		  log.warn("The entity name " + fromName
-			  + " contains illegal characters, it has been repaired to " + newName);
-		} else {
-		  // check for other less normal characters
-		  for (char c : fromName.trim().toCharArray()) {
-			final boolean normalChar = ('A' <= c && c <= 'Z') || ('0' <= c && c <= '9')
-				|| ('a' <= c && c <= 'z') || c == '_';
-			if (!normalChar) {
-			  log.warn("The entity name " + fromName + " contains a character (" + c
-				  + ") which could result in issues in HQL or "
-				  + "webservices. Use characters from a to z, A to Z or 0 to 9 or the _");
-			}
+		return nameChanged;
+	}
+
+	private void checksNotNormalChars(String fromName) {
+		for (char c : fromName.trim().toCharArray()) {
+		  final boolean normalChar = ('A' <= c && c <= 'Z') || ('0' <= c && c <= '9')
+			  || ('a' <= c && c <= 'z') || c == '_';
+		  if (!normalChar) {
+			  warnLogger("The entity name " + fromName + " contains a character (" + c
+					  + ") which could result in issues in HQL or "
+					  + "webservices. Use characters from a to z, A to Z or 0 to 9 or the _");
 		  }
 		}
-		return newName.toString();
+	}
+
+	private void warnLogger (String msg){
+		log.warn(msg);
 	  }
 
-
-	  public String getMethod (String name){
+  	public String getMethod (String name){
 		return removeIllegalChars(name);
 	  }
 
